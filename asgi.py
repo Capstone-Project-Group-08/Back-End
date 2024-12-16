7abedfae6d3daa083a15d2e32e1bd3b6b4bc328e
Sachintha Shehan <132140249+sachishehan@users.noreply.github.com>
2024-12-16 21:25:01 +0530

    asgi.py

--- asgi.py
+++ asgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
-import os
+import os # Standard library module to interact with the operating system
 
-from django.core.asgi import get_asgi_application
-
+from django.core.asgi import get_asgi_application # Function to get the ASGI application for Django
+# Set the default settings module for the 'myproject3' project
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject3.settings')
 
 application = get_asgi_application()
